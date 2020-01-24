db29ae4bc20d3a9f168125329121e6aab129ff92
Joseph Wangemann <fractalscape13@gmail.com>
2020-01-23 22:16:59 -0800

    add while loop to complete translation

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,44 @@
+//business logic
 var finalPhrase = [];
-
-function translateToPig(word) {
-  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  var letters = word.split("");
-
-  if (vowels.includes(letters[0])) {
-    finalPhrase.push(word + "way");
-  } else if (vowels.includes(letters[1])) {
-    var removedLetter = letters.shift();
-    var newWord1 = letters.concat(removedLetter + "ay").join("");
-    finalPhrase.push(newWord1);
-  } else if (vowels.includes(letters[2])) {
-    var removedLetter2 = letters.splice(0, 2);
-    finalPhrase.push(letters.join("") + removedLetter2.join("") + "ay");
-  }else if (vowels.includes(letters[3])) {
-    var removedLetter3 = letters.splice(0, 3);
-    var newWord = letters.concat(removedLetter3.join("") + "ay").join("");
-    finalPhrase.push(newWord);
-  } else {
-
-  }
-
- };
+//   var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//   var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
 
 
+  function translateToPig(word) {
+    var newStr = word;
+    if (newStr.slice(0,1).match(/[aeiouAEIOU]/)) {
+      newStr = newStr + "way";
+    }
+    else {
+      var moveLetters = "";
+      while (newStr.slice(0,1).match(/[^aeiouAEIOU]/)) {
+        moveLetters += newStr.slice(0,1);
+        newStr = newStr.slice(1, newStr.length);
+      }
+      newStr = newStr + moveLetters + "ay";
+    }
+    finalPhrase.push(newStr);
+  };
+    
+  
+  
+  // if (vowels.includes(letters[0])) {
+  //   finalPhrase.push(word + "way");
+  // } else if (vowels.includes(letters[1])) {
+  //   var removedLetter = letters.shift();
+  //   var newWord1 = letters.concat(removedLetter + "ay").join("");
+  //   finalPhrase.push(newWord1);
+  // } else if (vowels.includes(letters[2])) {
+  //   var removedLetter2 = letters.splice(0, 2);
+  //   finalPhrase.push(letters.join("") + removedLetter2.join("") + "ay");
+  // }else if (vowels.includes(letters[3])) {
+  //   var removedLetter3 = letters.splice(0, 3);
+  //   var newWord = letters.concat(removedLetter3.join("") + "ay").join("");
+  //   finalPhrase.push(newWord);
+  // }
 
-
-//pushes input through click function into translateToPig function^
+//user interface
 $(document).ready(function() {
   $("form.form").submit(function(event) {
     event.preventDefault();
